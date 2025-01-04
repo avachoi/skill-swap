9eddeb918cf56cb66ea59bcc44606e44c5b00f0f
avachoi <alexbchoi@gmail.com>
2025-01-04 18:48:18 -0500

    added signUP fetch

---
+++ frontend/js/signup.js
@@ -0,0 +1,53 @@
+const BASE_URL = "http://localhost:5000/api/users";
+
+const signupForm = document.getElementById("signupForm");
+
+console.log("js working");
+
+signupForm.addEventListener("submit", async (e) => {
+	e.preventDefault();
+
+	let userName = document.getElementById("name").value;
+	let email = document.getElementById("email").value;
+
+	let proficients = document.querySelectorAll(
+		"input[name='Proficient']:checked"
+	);
+
+	let checkedProficients = [];
+	proficients.forEach((prof) => {
+		checkedProficients.push(prof.value);
+	});
+
+	let requireds = document.querySelectorAll("input[name='required']:checked");
+
+	let checkedRequireds = [];
+	requireds.forEach((req) => {
+		checkedRequireds.push(req.value);
+	});
+	console.log(checkedProficients, checkedRequireds);
+
+	let user = {
+		name: userName,
+		email: email,
+		skillsProficient: checkedProficients,
+		skillsNeeded: checkedRequireds,
+	};
+	try {
+		const response = await fetch(`${BASE_URL}/signup`, {
+			method: "POST",
+			headers: { "Content-Type": "application/json" },
+			body: JSON.stringify(user),
+		});
+		if (response.ok) {
+			alert("Signed up successfully!");
+			// fetchMatches(email); // Fetch matches after signup
+		} else {
+			const error = await response.json();
+			alert(`Error: ${error.message}`);
+		}
+	} catch (err) {
+		console.error("Error signing up:", err);
+		alert("An error occurred. Please try again.");
+	}
+});
